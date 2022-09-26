2caaef7a005fc47ed75391c6322507c858a7bb3f
Victor Di Iulio <victor_iulio@outlook.com>
2022-09-26 10:44:13 -0300

    Primeira função em C teste

---
+++ funcoes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// tipoDeRetorno nomeDaFuncao (listagem de parametros)
+    {
+      //  instruçoes;
+      //  retuno
+    }
+
+
+
+void imprima(void) {
+    printf("testando");
+}
+
+int main(){
+    // (declare sempre)
+    void imprima(void);
+    //chame a função
+    imprima();
+
+}
